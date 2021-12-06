0af8a2941aba85820737ad918e1e415ef26c0dcb
yussifm13 <yussifm13@gmail.com>
2021-12-06 00:28:47 +0000

    list

---
+++ comprehension_list.py
@@ -0,0 +1,13 @@
+# # Simple Lc
+
+# square = [x*x for x in (1,2,3,4,5,6,7,8,9)]
+# print(square)
+
+
+# # Pring List of upper char from str
+# print([s.upper() for s in "hello world"])
+
+# print([s.strip(',') for s in ["Helo," "world,", "coded,", "studio,"]])
+
+# DIctionay Comprehension
+print({x: x*x for x in (1,2,3,4,5)})
